c11669054f35afaa20220522fc26181073243335
nlecherf <noemie.lecherf@epsi.fr>
2020-09-10 09:37:31 +0200

    push sans erreur

--- src/controleur/controleur_index.php
+++ src/controleur/controleur_index.php
@@ -4,14 +4,6 @@ function actionAccueil($twig) {
     echo $twig->render('index.html.twig', array());
 }
 
-function actionConnexion($twig){
-    echo $twig->render('connexion.html.twig',array());
-}
-
-function actionInscription($twig){
-    echo $twig->render('inscription.html.twig', array());
-}
-
 function actionConsommation($twig){
     echo $twig->render('MaConso.html.twig', array());
 }
